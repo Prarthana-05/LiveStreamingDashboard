a310613ec3804f0153e28bfe714b3d764dd7e90f
Prarthana Ingawale <prarthana.ingawale.st@gmail.com>
2026-08-15 13:57:01 +0530

    Extend LiveStreamingDashboardApplication with SpringBootServletInitializer

--- src/main/java/com/prarthana/livestreamingdashboard/LiveStreamingDashboardApplication.java
+++ src/main/java/com/prarthana/livestreamingdashboard/LiveStreamingDashboardApplication.java
@@ -2,12 +2,13 @@ package com.prarthana.livestreamingdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LiveStreamingDashboardApplication {
+public class LiveStreamingDashboardApplication
+        extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LiveStreamingDashboardApplication.class, args);
     }
-
 }
